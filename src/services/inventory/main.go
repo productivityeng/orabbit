9776f60dcbbcfe6f09d313e8c6e1aec5520c3700
victornascimento <victooyooth@gmail.com>
2023-08-05 17:54:44 -0300

    disabling husky from ci

--- src/services/inventory/main.go
+++ src/services/inventory/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	broker_controller "github.com/productivityeng/orabbit/broker/controllers"
+	"github.com/productivityeng/orabbit/broker/entities"
 	"github.com/productivityeng/orabbit/broker/repository"
 	"github.com/productivityeng/orabbit/core/validators"
 	database_mysql "github.com/productivityeng/orabbit/database"
 	"github.com/productivityeng/orabbit/docs"
 	"github.com/productivityeng/orabbit/src/packages/rabbitmq"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	broker_controller "github.com/productivityeng/orabbit/broker/controllers"
-	"github.com/productivityeng/orabbit/broker/entities"
 	log "github.com/sirupsen/logrus"
 
 	swaggerfiles "github.com/swaggo/files"
@@ -34,19 +32,13 @@ func main() {
 	brokerRepository = repository.NewBrokerMysqlImpl(database_mysql.Db)
 	brokerValidator = validators.NewBrokerValidatorDefault(brokerRepository, overviewManagement)
 	brokerController = broker_controller.NewBrokerController(brokerRepository, brokerValidator)
-
+	brokerResourcePath := "/broker/:brokerId"
 	r.GET("/broker", brokerController.ListBrokers)
 	r.GET("/broker/exists", brokerController.FindBroker)
-	r.GET("/broker/:brokerId", brokerController.GetBroker)
-	r.PUT("/broker/:brokerId", brokerController.UpdateBroker)
+	r.GET(brokerResourcePath, brokerController.GetBroker)
+	r.PUT(brokerResourcePath, brokerController.UpdateBroker)
 	r.POST("/broker", brokerController.CreateBroker)
-	r.DELETE("/broker/:brokerId", brokerController.DeleteBroker)
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.DELETE(brokerResourcePath, brokerController.DeleteBroker)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	log.Fatal(r.Run(":8082"))
