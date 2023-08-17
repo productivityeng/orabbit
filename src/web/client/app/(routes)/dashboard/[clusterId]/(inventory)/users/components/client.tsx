4b4331e9c91465551db34918ad7c0c6934c6e170
victornascimento <victooyooth@gmail.com>
2023-08-16 21:28:42 -0300

    Add translation for local header of users list

--- src/web/client/app/(routes)/dashboard/[clusterId]/(inventory)/users/components/client.tsx
+++ src/web/client/app/(routes)/dashboard/[clusterId]/(inventory)/users/components/client.tsx
@@ -8,17 +8,19 @@ import { UserColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 import Heading from "@/components/Heading/Heading";
 import SimpleHeading from "@/components/Heading/SimpleHeading";
+import { useTranslations } from "next-intl";
 
 interface UsersClientProps {
   data: UserColumn[];
 }
 
 function UsersClient({ data }: UsersClientProps) {
+  const t = useTranslations();
   return (
     <div>
       <SimpleHeading
-        title="Users watched"
-        description="Users that are registed in ostern"
+        title={t("UsersPage.TrackedUsers")}
+        description={t("UsersPage.TrackedUsersDescription")}
       />
       <DataTable searchKey="username" columns={columns} data={data} />
     </div>
